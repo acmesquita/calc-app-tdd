82c3d338524034795890b009b742b2b095c7beee
Catharina Mesquita <catha.ana.1994@gmail.com>
2022-01-14 15:17:56 -0300

    refactor: change evaluate method
    Co-authored-by: Thiago Henrique de A.C. Santos <thiagohenriquesantos@users.noreply.github.com>

--- src/models/calculate.spec.ts
+++ src/models/calculate.spec.ts
@@ -1,12 +1,22 @@
 /* eslint-disable no-eval */
 export class Calculate {
+  
   evaluate(expression: string): string {
-    return String(eval(expression)).substring(0, 14)
+    const result = eval(expression)
+       
+    return this.formatResult(result)
+  }
+  
+  private formatResult(value: number): string {
+    const MAX_LENGTH = 14
+    const valueString = value.toString()
+    const valueLimited = valueString.substring(0, MAX_LENGTH)
+    return valueLimited;
   }
 }
 
 describe('Calculate', () => {
-  test('Should return 2 when initilize Calculate with expression 1 + 1', () => {
+  test('Should return 2 when call evaluate with expression 1 + 1', () => {
     const calculate = new Calculate()
     const expression = "1 + 1"
 
@@ -15,7 +25,7 @@ describe('Calculate', () => {
     expect(result).toBe("2")
   })
 
-  test('Should return 6 when initilize Calculate with expression 2 * 3', () => {
+  test('Should return 6 when call evaluate with expression 2 * 3', () => {
     const calculate = new Calculate()
     const expression = "2 * 3"
 
@@ -24,7 +34,7 @@ describe('Calculate', () => {
     expect(result).toBe("6")
   })
 
-  test('Should return 5 when initilize Calculate with expression 10 - 5', () => {
+  test('Should return 5 when call evaluate with expression 10 - 5', () => {
     const calculate = new Calculate()
     const expression = "10 - 5"
 
@@ -33,7 +43,7 @@ describe('Calculate', () => {
     expect(result).toBe("5")
   })
 
-  test('Should return Infinity when initilize Calculate with expression 10 / 0', () => {
+  test('Should return Infinity when call evaluate with expression 10 / 0', () => {
     const calculate = new Calculate()
     const expression = "10 / 0"
 
@@ -42,7 +52,7 @@ describe('Calculate', () => {
     expect(result).toBe("Infinity")
   })
 
-  test('Should return 0.333333333333 when initilize Calculate with expression 1 / 3', () => {
+  test('Should return 0.333333333333 when call evaluate with expression 1 / 3', () => {
     const calculate = new Calculate()
     const expression = "1 / 3"
 
