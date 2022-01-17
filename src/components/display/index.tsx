3db4cf0bda4d42e332e21ac9e83b6830ed9a59f2
Catharina Mesquita <catha.ana.1994@gmail.com>
2022-01-17 12:26:39 -0300

    feat: add value props

--- src/components/display/index.tsx
+++ src/components/display/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import style from '../../styles/components/display.module.css'
 
-const Display: React.FC = () => {
+type Prop = {
+  value: string
+}
+
+const Display: React.FC<Prop> = ({ value }: Prop) => {
   return (
-    <div className={style.displayWrapper} data-testid="display">1234</div>
+    <div className={style.displayWrapper} data-testid="display">
+      {value}
+    </div>
   )
 }
 
